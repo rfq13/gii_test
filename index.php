c1312cee5ecfc405295bb00841b831ae2f2384d3
Muhammad Rifqy <hadimrifqyfakhrul@gmail.com>
2021-08-25 10:45:48 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$x=8;
+for ($i = 1; $i <= $x; $i++) {
+	for ($j = 1; $j <= $i ; $j++) {
+		echo $j;
+	}
+	echo '<br>';
+}
+
+$jumlah=5;
+for($a=1; $a<=$jumlah; $a++){
+    for($b=$jumlah; $b>=$a; $b--){
+        print('&nbsp');
+    }
+    for($c=1; $c<=$a; $c++){
+        echo '*';
+    }
+    echo "<br/>";
+}
